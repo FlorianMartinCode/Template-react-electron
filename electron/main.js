f2d71ff172a32b78ffcbb58b477d8d58c6d303b0
Martin <florian.martin700@gmail.com>
2024-10-27 12:07:18 +0100

    First commit

---
+++ electron/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'), // Si vous utilisez un fichier preload
+        },
+    });
+
+    win.loadURL('http://localhost:3000'); // Charge votre application React en développement
+
+    // Ouvrir les outils de développement (DevTools)
+    win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
